b9bab9701be1459e360d87c39b16d9aaa843e644
Miguel Zacca <143193845+miguelzacca@users.noreply.github.com>
2025-01-21 15:34:45 -0300

    Update dialog.ps1

--- dialog.ps1
+++ dialog.ps1
@@ -1,7 +1,7 @@
 Add-Type -AssemblyName Microsoft.VisualBasic
 
 do {
-    $result = [Microsoft.VisualBasic.Interaction]::InputBox("Puta?", "Brendinha", "Sim ou Não")
-} while ($result -eq "") 
+    $result = [Microsoft.VisualBasic.Interaction]::InputBox("Fala alguma coisa ai brendinha", "Brendinha", "Digite aqui")
+} while ($result -eq "")  
 
 Invoke-RestMethod -Uri "http://192.168.1.40:3000/recv" -Method Post -ContentType "text/plain" -Body $result
